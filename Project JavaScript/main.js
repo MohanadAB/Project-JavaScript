f62ed236c151984979fe82b9217ad10a6244c78e
DeadDevilÓ™☕ <54993284+MohanadAB@users.noreply.github.com>
2019-10-29 04:03:42 +0200

    Update main.js

--- Project JavaScript/main.js
+++ Project JavaScript/main.js
@@ -9,36 +9,37 @@ body.style.backgroundRepeat= "no-repeat";
 
 let splash = document.querySelector('#splash')
 		splash.addEventListener('click', () =>{
-			document.body.style.backgroundImage = "url('splash.jpg')";
+		     document.body.style.backgroundImage = "url('splash.jpg')";
 		});
 let splashSword = document.querySelector('#splashSword')
-		splashSword.addEventListener('click', () =>{
+		     splashSword.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashSword.jpg')";
 		});
 let splasGhost = document.querySelector('#splashGhost')
-		splashGhost.addEventListener('click', () =>{
+		    splashGhost.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashGhost.jpg')";
 		});
 let splasFreedom = document.querySelector('#splashFreedom')
-		splashFreedom.addEventListener('click', () =>{
+		     splashFreedom.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashFreedom.jpg')";
 		});
 let splashSaber = document.querySelector('#splashSaber')
-		splashSaber.addEventListener('click', () =>{
+		    splashSaber.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashSaber.jpg')";
 		});
 let splasRobote = document.querySelector('#splashRobote')
-		splashRobote.addEventListener('click', () =>{
+		     splashRobote.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashRobote.jpg')";
 		});
 let splasDeath = document.querySelector('#splashDeath')
-		splashDeath.addEventListener('click', () =>{
+		     splashDeath.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashDeath.jpg')";
 		});
 let splasNier = document.querySelector('#splashNier')
-		splashNier.addEventListener('click', () =>{
+		    splashNier.addEventListener('click', () =>{
 			document.body.style.backgroundImage = "url('splashNier.jpg')";
 		});
+
 // body.style.padding = "10px";
 
 var W = window.innerWidth;
@@ -47,37 +48,36 @@ console.log(pixW);
 
 var H = window.innerHeight;
 var pixH= Math.floor(H/40);
-
 body.style.marginTop=`${(H%40)/2}px`;
 body.style.marginLeft=`${(W%40)/2}px`;
-
 console.log(pixH);
+
 var temp = "";
 for (let a = 0; a < pixW; a++) {
     temp+="40px " 
 }
 body.style.gridTemplateColumns=temp;
+
 var rows = "";
 for (let a = 0; a < pixH; a++) {
     rows+="40px " 
 }
 body.style.gridTemplateRows=rows;
+
 // body.style.gridTemplateRows=temp;
 // console.log(temp);
+
 for (var i = 0; i < pixW*pixH; i++) {
-    let D = document.createElement("div");
-	 
+    let D = document.createElement("div"); 
     D.addEventListener("mouseenter",()=>{
         console.log("aa")
         D.style.backgroundColor="#"+((1<<24)*Math.random()|0).toString(16);
     });
-	
     D.addEventListener("onmousemove",()=>{
         console.log("aa")
         D.style.backgroundColor="";
     });
-	
-		D.style.transition = "all 0.1s linear 0s";
+	D.style.transition = "all 0.1s linear 0s";
     body.appendChild(D);
     // D.innerText=i;
     console.log(i)
